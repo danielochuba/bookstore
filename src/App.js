240e2416ad7533efaa0dae7dfd6419609f3b71e0
Daniel Ochuba <danielochuba78@gmail.com>
2023-06-22 17:14:58 +0100

    create books and categories component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './components/books'
+import Categories from './components/categories'
 import './App.css';
+
+function Layout() {
+  return (
+    <header className="App-header">
+    <h1>Bookstore CMS</h1>
+    <nav className="navLinks">
+      <Link to="/" element={<Home />} />
+      <Link path="/categories" element={<Categories />} />
+    </nav>        
+  </header>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path='/' element={<Home />} >
+          <Route path='/categories' element={<Categories />} />
+        </Route>
+      </Routes>
+      
     </div>
   );
 }
